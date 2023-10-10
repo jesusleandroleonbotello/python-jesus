c524718da843c8f8b3e154f40e01bab803af7fa1
jesusleandroleonbotello <ljesusleandro@gmail.com>
2023-10-10 08:30:09 -0500

    parte dos

---
+++ archivos/leer_txt.py
@@ -0,0 +1,5 @@
+#vamos a importar un archivo que guardamos en esta carpeta
+
+#archivo = open("archivos\\prueba_texto.txt")
+
+#print(archivo.read())
--- temas_video_dalto_INTERMEDIO/paquete-GOD/packs.py
+++ temas_video_dalto_INTERMEDIO/paquete-GOD/packs.py
@@ -1,6 +1,5 @@
 # vamos a importar un paquete estos contienen varios archivos un paquete seria una carpeta donde hay muchos archivos
 # recordar qu parapoder importar un paquete debe tener un archibo llamado __init__.py  no es necesario tener nada en ese archivo
 
-import MODULOS
-
-#print(zzzz_saludar.saludar("juan"))
+#import MODULOS.zzzz_saludar
+#print(zzzz_saludar.saludar("juan")) aqui importamos saludar
