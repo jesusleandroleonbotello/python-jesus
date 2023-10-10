da3f24b41f6ff24100fca3c3482a7a0021f3d306
jesusleandroleonbotello <ljesusleandro@gmail.com>
2023-10-10 08:12:31 -0500

    feat(partes nuevas): :sparkles: sobre modulos
    importat

---
+++ MODULOS/zzzz _saludar.py
@@ -0,0 +1,8 @@
+
+
+def saludar(name):
+    return f"hola {name} como estas"
+
+print(__name__)
+
+
---
+++ temas_video_dalto_INTERMEDIO/modulos2.0/importar.py
@@ -0,0 +1,26 @@
+# aqui vamos a importar de culaquer archivo como ? pues colocando el nobre del file o del arcvhivo donde esta el file seguido de un punto . y el nombre del file
+# si el modulo estuviera en la carpeta
+import saludar as m_saludar
+
+print(m_saludar.saludar("juan"))
+
+# si estuviera en la misma ruta pero fuera de la carpeta seria asi
+
+#import funciones.crear_funciones 
+
+#import sys # esto es un modulo de python
+
+#print(sys.builtin_module_names) # con esto nos muestra una tupla de todos los valores de las propiedades
+
+# recordar que al momento de crear un modulo debe ser diferntes a los de python ya que este le da prioridad a los modulos nativos y no al nuestro
+
+#import sys
+
+#print(sys.path) #['/home/Exegol-164/Documents/git jesus/python-jesus/temas_video_dalto_INTERMEDIO/modulos2.0'
+# actualmete esta es nuestra ruta 
+
+# ahora vamos a acceder a una carpeta que esta fuera de nuestra ruta
+
+#import sys.append("/home/Exegol-164/Documents/git jesus/python-jesus/ejercicios_practicos_1")
+
+#print(sys)#
